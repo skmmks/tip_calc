d7c21a17274da78f769c617ba57971f3920ea316
Steve Min <stevekmin@gmail.com>
2020-01-20 17:33:38 +0000

    remove console logs

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-console.log("main is connected");
 var calcTip = () => {
   let totalBill = document.getElementById("totalbill").value;
   let serviceQuality = document.getElementById("serviceQuality").value;
   let totalPeople = document.getElementById("totalPeople").value;
 };
 
-document.getElementById("calculate").onclick = () => {
-  console.log("button was clicked");
-};
+document.getElementById("calculate").onclick = () => {};
