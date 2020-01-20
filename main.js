3f1c0f1bfe4b40879377926953cf13a496c8d74c
Steve Min <stevekmin@gmail.com>
2020-01-20 17:22:47 +0000

    define serviceQuality variable define totalPeople variable

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 console.log("main is connected");
 var calcTip = () => {
   let totalBill = document.getElementById("totalbill").value;
+  let serviceQuality = document.getElementById("serviceQuality").value;
+  let totalPeople = document.getElementById("totalPeople").value;
 };
