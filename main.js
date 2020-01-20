877f1d64e82d21c1204cb36745f5b9f407f9c10f
Steve Min <stevekmin@gmail.com>
2020-01-20 17:33:02 +0000

    move js file to body on load define onclick action on js file

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ var calcTip = () => {
   let serviceQuality = document.getElementById("serviceQuality").value;
   let totalPeople = document.getElementById("totalPeople").value;
 };
+
+document.getElementById("calculate").onclick = () => {
+  console.log("button was clicked");
+};
