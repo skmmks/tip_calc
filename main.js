1c2f62f30c5904148665c21487c61e5e07bdf4e8
Steve Min <stevekmin@gmail.com>
2020-01-21 17:44:24 +0000

    manipulate value of total variable to show dollar and cent format

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var calcTip = () => {
   let serviceQuality = document.getElementById("serviceQuality").value;
   let totalPeople = document.getElementById("totalPeople").value;
   let totalTip = (totalBill * serviceQuality) / totalPeople;
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
 };
 
 document.getElementById("calculate").onclick = () => {};
