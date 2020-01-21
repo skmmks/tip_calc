16d1a6592cc2fce03fd422490d24b41cae1198c0
Steve Min <stevekmin@gmail.com>
2020-01-21 17:42:55 +0000

    define totalTip variable

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var calcTip = () => {
   let totalBill = document.getElementById("totalbill").value;
   let serviceQuality = document.getElementById("serviceQuality").value;
   let totalPeople = document.getElementById("totalPeople").value;
+  let totalTip = (totalBill * serviceQuality) / totalPeople;
 };
 
 document.getElementById("calculate").onclick = () => {};
